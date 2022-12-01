0957107ad95a781b92c1ef13af8d035fe9626fff
Jacob Heider <jacob@tea.xyz>
2022-12-01 16:47:39 -0500

    keep ~/.cabal clean

--- gha-runner/pre-run.sh
+++ gha-runner/pre-run.sh
@@ -2,3 +2,6 @@
 
 find /opt -mindepth 1 -maxdepth 1 -print0 | xargs -0 rm -rf
 rm -rf "$GITHUB_WORKSPACE" && mkdir "$GITHUB_WORKSPACE"
+
+# some tools like $HOME a little too much
+test -d "$HOME"/.cabal && rm -rf "$HOME"/.cabal
