da6ab51f4221022b3dd0729a2f7c0e2a762c8261
Jacob Heider <jacob@tea.xyz>
2022-12-21 20:59:11 -0500

    shellcheck

--- gha-runner/pre-run.sh
+++ gha-runner/pre-run.sh
@@ -4,4 +4,7 @@ find /opt -mindepth 1 -maxdepth 1 -print0 | xargs -0 rm -rf
 rm -rf "$GITHUB_WORKSPACE" && mkdir "$GITHUB_WORKSPACE"
 
 # some tools like $HOME a little too much
-test -d "$HOME"/.cabal && rm -rf "$HOME"/.cabal || true
+if test -d "$HOME"/.cabal
+then
+  rm -rf "$HOME"/.cabal
+fi
