0080b4e4d544e63793c9bf334ef8695ffec93aaa
Jacob Heider <jacob@tea.xyz>
2023-01-04 20:27:26 -0500

    go leaves behind read-only artifacts in /opt

--- gha-runner/pre-run.sh
+++ gha-runner/pre-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find /opt -mindepth 1 -maxdepth 1 -print0 | xargs -0 rm -rf
+find /opt -mindepth 1 -maxdepth 1 -print0 | xargs -0 sudo rm -rf
 rm -rf "$GITHUB_WORKSPACE" && mkdir "$GITHUB_WORKSPACE"
 
 # some tools like $HOME a little too much
