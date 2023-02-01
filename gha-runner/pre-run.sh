fc30df282f5fda0fec44ed208d05c27890110752
Jacob Heider <jacob@tea.xyz>
2023-02-01 15:35:50 -0500

    add more stuff to remove

--- gha-runner/pre-run.sh
+++ gha-runner/pre-run.sh
@@ -4,10 +4,9 @@ find /opt -mindepth 1 -maxdepth 1 -print0 | xargs -0 sudo rm -rf
 rm -rf "$GITHUB_WORKSPACE" && mkdir "$GITHUB_WORKSPACE"
 
 # some tools like $HOME a little too much
-if test -d "$HOME"/.cabal; then
-  rm -rf "$HOME"/.cabal || true
-fi
-
-if test -d "$HOME"/opt; then
-  rm -rf "$HOME"/opt || true
-fi
+TO_CLEAN=(.cabal opt .tea)
+for DIR in "${TO_CLEAN[@]}"; do
+  if test -d "$HOME"/"$DIR"; then
+    rm -rf "${HOME:?}"/"${DIR:?}" || true
+  fi
+done
