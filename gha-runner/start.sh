edc51fde3560c7bcb07945e567d60196ba6a0a4f
Jacob Heider <jacob@tea.xyz>
2023-03-17 19:05:40 -0400

    clean temp directory

--- gha-runner/start.sh
+++ gha-runner/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+set -e
+
+sudo service rsyslog start
+sudo service cron start
+
 # With thanks to https://testdriven.io/blog/github-actions-docker/
 
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/"${ORGANIZATION}"/actions/runners/registration-token | jq .token --raw-output)
