0674748accfefb2780a6903fa14cbfe317d9546b
Jacob Heider <jacob@tea.xyz>
2023-09-30 09:22:08 -0400

    include pkgx

--- gha-runner/pre-run.sh
+++ gha-runner/pre-run.sh
@@ -4,7 +4,7 @@ find /opt -type d -mindepth 1 -maxdepth 1 -print0 | xargs -0 sudo rm -rf
 rm -rf "$GITHUB_WORKSPACE" && mkdir "$GITHUB_WORKSPACE"
 
 # some tools like $HOME a little too much
-TO_CLEAN=(.cabal opt .tea)
+TO_CLEAN=(.cabal opt .tea .pkgx)
 for DIR in "${TO_CLEAN[@]}"; do
   if test -d "$HOME"/"$DIR"; then
     rm -rf "${HOME:?}"/"${DIR:?}" || true
@@ -13,4 +13,5 @@ done
 
 find /tmp/ -mindepth 1 -maxdepth 1 -type d -name '????????' -exec sudo rm -rf {} \;
 find /tmp/ -mindepth 1 -maxdepth 1 -type d -name 'xyz.tea.*' -exec sudo rm -rf {} \;
+find /tmp/ -mindepth 1 -maxdepth 1 -type d -name 'dev.pkgx.*' -exec sudo rm -rf {} \;
 find /tmp/ -mindepth 1 -maxdepth 1 -type d -name '??????' -exec sudo rm -rf {} \;
