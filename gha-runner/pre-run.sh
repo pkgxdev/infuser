a1d95c9d507d5615ea575323dbbd8886006d8c2e
Jacob Heider <jacob@tea.xyz>
2023-10-01 23:32:32 -0400

    gotta be sudo rm throughout

--- gha-runner/pre-run.sh
+++ gha-runner/pre-run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 find /opt -type d -mindepth 1 -maxdepth 1 -print0 | xargs -0 sudo rm -rf
-rm -rf "$GITHUB_WORKSPACE" && mkdir "$GITHUB_WORKSPACE"
+sudo rm -rf "$GITHUB_WORKSPACE" && mkdir "$GITHUB_WORKSPACE"
 
 # some tools like $HOME a little too much
 TO_CLEAN=(.cabal opt .tea .pkgx)
 for DIR in "${TO_CLEAN[@]}"; do
   if test -d "$HOME"/"$DIR"; then
-    rm -rf "${HOME:?}"/"${DIR:?}" || true
+    sudo rm -rf "${HOME:?}"/"${DIR:?}" || true
   fi
 done
 
